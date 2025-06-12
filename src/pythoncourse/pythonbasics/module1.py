be740ae86b3f6428964c653c924fd542927e7116
Marco Larotonda <marcolarotonda@kubepartners.com>
2025-06-12 10:44:33 +0200

    esercizi python basics - modulo 1

---
+++ src/pythoncourse/pythonbasics/module1.py
@@ -0,0 +1,39 @@
+def convert_miles_to_km():
+    """
+    Scrivi una funzione che accetta un numero che rappresenta una distanza in miglie.
+    Convertilo in km e ritornalo.
+    """
+    pass
+
+
+def sum():
+    """
+    Scrivi una funzione che accetta 2 numeri come input, e ritorna la loro somma.
+    Il valore ritornato deve essere un numero intero.
+    """
+    pass
+
+
+def division():
+    """
+    Scrivi una funzione che accetta 2 numeri come input, e ritorna la loro somma.
+    Il valore ritornato deve essere un numero intero.
+    """
+    pass
+
+
+def convert_to_hours_and_minutes():
+    """
+    Scrivi una funzione che accetta un numero intero come input che rappresenta una
+    certa quantità di minuti. Converti questa quantità in ore e minuti.
+    Ritorna una stringa formattata come "ore:minuti"
+    """
+    pass
+
+
+def is_even():
+    """
+    Scrivi una funzione che accetta un numero intero e ritorna True se è pari,
+    altrimenti False.
+    """
+    pass
