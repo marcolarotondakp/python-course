4116172d9c0051cec2b88766549b54dc1a050d06
Marco Larotonda <marcolarotonda@kubepartners.com>
2025-06-18 17:46:12 +0200

    esercizi python basics - modulo 2

---
+++ src/pythoncourse/pythonbasics/module2.py
@@ -0,0 +1,49 @@
+def ottieni_sublist():
+    """
+    Input: una lista.
+    Output: una sublist ottenuta togliendo il primo e l'ultimo elemento.
+    Esempio: [4,3,5,2,1] -> [3,5,2].
+    """
+    pass
+
+
+def filtra_lista_di_stringhe():
+    """
+    Input: una lista di stringhe.
+    Output: una lista che contiene solo le stringhe ricevute in input che hanno
+    un numero di caratteri multiplo di 3.
+    Esempio: ["ciao", "dai", "bistecca", "poi", "pancia"] -> ["dai", "poi",  "pancia"].
+    """
+    pass
+
+
+def inverti_parole():
+    """
+    Input: una stringa.
+    Output: la stessa stringa, ma con l'ordine delle lettere di ciascuna parola
+    invertito.
+    Esempio: "Hello World" -> "olleH dlroW".
+    """
+    pass
+
+
+def riduci_lista_di_interi_positivi():
+    """
+    Input: una lista di interi positivi.
+    Output: un numero intero calcolato sommando i numeri della lista che hanno indice pari,
+    e sottraendo quelli che hanno indice dispari.
+    Esempio: [4,2,3,1,5] -> 4-2+3-1+5.
+    """
+    pass
+
+
+def carattere_piu_frequente():
+    """
+    Esercizio difficile!
+    Input: una lista di stringhe da un carattere ciascuna.
+    Output: il carattere più frequente. Se più caratteri hanno lo stesso numero di
+    occorrenze, ritorna quello che ha indice più basso.
+    Esempio: ["a", "b", "a", "c", "c"] -> "a" (il carattere "c" compare in un indice più alto di "a").
+    Hint: puoi usare l'operatore "maggiore di" >
+    """
+    pass
