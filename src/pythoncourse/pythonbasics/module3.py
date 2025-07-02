11a3cd76caea6ab904459c211039333a38daab68
Marco Larotonda <marcolarotonda@kubepartners.com>
2025-07-02 12:12:43 +0200

    esercizi python basics - modulo 3

---
+++ src/pythoncourse/pythonbasics/module3.py
@@ -0,0 +1,41 @@
+def controlla_substring():
+    """
+    Input: due stringhe
+    Output: booleano
+    Ritorna True se la prima stringa è contenuta nella seconda, altrimenti False
+    """
+    pass
+
+def controlla_anno_bisestile():
+    """
+    Input: un intero positivo che rappresenta un anno
+    Output: booleano
+    Ritorna True se l'anno è bisestile, altrimenti False
+    Un anno è bisestile se è divisibile per 4, ma non per 100, eccetto se è anche divisibile per 400.
+    """
+    pass
+
+def converti_voto_in_lettera():
+    """
+    Input: 0 <= n <= 100 
+    Output: una delle seguenti lettere, in base al voto n
+        A: 90-100
+        B: 80-89
+        C: 70-79
+        D: 60-69
+        F: 0-59
+    """
+    pass
+
+def morra_cinese():
+    """
+    Input: 2 stringhe, la prima per il giocatore1, e la seconda per il giocatore2.
+    I valori possibili di input sono le seguenti stringhe, che rappresentano
+    le 3 opzioni della morra cinese (sasso, carta, e forbice): s, c, f.
+    Output: un numero intero in base al risultato della partita.
+        1: vittoria del giocatore1
+        0: vittoria del giocatore2
+        -1: pareggio
+    Hint: per una soluziona particolarmente elegante usa l'operatore %.
+    """
+    pass
